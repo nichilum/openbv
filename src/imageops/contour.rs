88afe314c1f6a380733304f2439137d360e419ee
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2024-05-14 18:12:20 +0200

    joa

--- src/imageops/contour.rs
+++ src/imageops/contour.rs
@@ -1,4 +1,4 @@
-use image::GenericImageView;
+use image::{GenericImageView, GrayImage};
 
 use super::binarize::BinaryImage;
 
@@ -11,26 +11,98 @@ pub struct Moments;
 pub trait ContourExt {
     fn find_contours(&self) -> Vec<Contour>;
     fn trace_contour(&self, x: u32, y: u32) -> Contour;
+    fn handle_fg_pixel(
+        &self,
+        output_img: &mut GrayImage,
+        x: u32,
+        y: u32,
+        label: &mut u8,
+        r: &mut u8,
+        outer_contours: &mut Vec<Contour>,
+    );
+    fn handle_bg_pixel(
+        &self,
+        output_img: &mut GrayImage,
+        x: u32,
+        y: u32,
+        label: &mut u8,
+        r: &mut u8,
+        inner_contours: &mut Vec<Contour>,
+    );
 }
 
 impl ContourExt for BinaryImage {
     fn find_contours(&self) -> Vec<Contour> {
         let img = &self.0;
-        let mut contours = vec![];
+        let mut output_img = self.0.clone();
+        let mut outer_contours = vec![];
+        let mut inner_contours = vec![];
 
+        let mut r = 1;
         for y in 0..img.dimensions().1 {
+            let mut label = 0;
             for x in 0..img.dimensions().0 {
                 if img.get_pixel(x, y)[0] == 255 {
-                    contours.push(self.trace_contour(x, y));
-                    return contours;
+                    self.handle_fg_pixel(
+                        &mut output_img,
+                        x,
+                        y,
+                        &mut label,
+                        &mut r,
+                        &mut outer_contours,
+                    );
+                } else {
+                    self.handle_bg_pixel(
+                        &mut output_img,
+                        x,
+                        y,
+                        &mut label,
+                        &mut r,
+                        &mut inner_contours,
+                    );
                 }
             }
         }
 
-        contours
+        outer_contours
     }
 
     fn trace_contour(&self, x: u32, y: u32) -> Contour {
+        todo!()
+    }
+
+    fn handle_fg_pixel(
+        &self,
+        output_img: &mut GrayImage,
+        x: u32,
+        y: u32,
+        mut label: &mut u8,
+        r: &mut u8,
+        outer_contours: &mut Vec<Contour>,
+    ) {
+        if *label != 0 {
+            output_img.put_pixel(x, y, image::Luma([*label]))
+        } else {
+            label = &mut output_img.get_pixel(x, y)[0];
+            if *label == 0 {
+                *r += 1;
+                label = r;
+                let contour = self.trace_contour(x, y);
+                outer_contours.push(contour);
+                output_img.put_pixel(x, y, image::Luma([*label]))
+            }
+        }
+    }
+
+    fn handle_bg_pixel(
+        &self,
+        output_img: &mut GrayImage,
+        x: u32,
+        y: u32,
+        label: u8,
+        r: u32,
+        inner_contours: &mut Vec<Contour>,
+    ) {
         todo!()
     }
 }
