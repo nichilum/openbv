38b7905e2fc51f9868953552d00ec34a031460c5
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2024-05-15 23:24:48 +0200

    cleanup

--- examples/hull.rs
+++ examples/hull.rs
@@ -1,18 +1,11 @@
-use std::fmt::Binary;
-
 use openbv::{
-    imageops::{
-        binarize::{BinarizeExt, BinaryImage},
-        contour::ContourExt,
-        dilate::DilateExt,
-        erode::ErodeExt,
-    },
+    imageops::{binarize::BinarizeExt, contour::ContourExt, dilate::DilateExt, erode::ErodeExt},
     math::{kernel::PLUS_FILTER, thresholds::ThresholdExt},
     open_gray,
 };
 
 fn main() {
-    let image = open_gray("./images/test.png").unwrap();
+    let image = open_gray("./images/Set03_single.jpg").unwrap();
 
     let otsu_thresh = image.otsu().unwrap();
     let binary_img = image.binarize(otsu_thresh);
@@ -23,21 +16,17 @@ fn main() {
     let (mut inner_contours, mut outer_contours) = dilated_img.find_contours();
     inner_contours.retain(|c| c.contour_area() > 10);
     outer_contours.retain(|c| c.contour_area() > 10);
-    // inner_contours.append(&mut outer_contours);
+    inner_contours.append(&mut outer_contours);
 
     // let convex_hulls = outer_contours
     //     .iter()
     //     .map(|c| c.convex_hull())
     //     .collect::<Vec<_>>();
 
-    let poly_hulls = outer_contours
+    let poly_hulls = inner_contours
         .iter()
-        .map(|c| c.poly_hull(10.9))
+        .map(|c| c.poly_hull(6.))
         .collect::<Vec<_>>();
-
-    // for hull in &poly_hulls {
-    //     println!("{:?}", hull.0);
-    // }
 
     let hull_img = dilated_img.draw_hulls(poly_hulls);
     hull_img.save("poly_hulls.png").unwrap();
