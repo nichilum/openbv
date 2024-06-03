3af9d5f1d00cfb4c9252b2aa2ccc3fd52484219a
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2024-06-03 23:54:37 +0200

    correct break point

--- examples/median_cut.rs
+++ examples/median_cut.rs
@@ -7,39 +7,49 @@ fn main() {
 
     let hist = image.histogram();
 
-    let bucket_count = 8;
+    // must be 2^i
+    let bucket_count = 2usize.pow(3);
 
-    let mut bucket_edges: Vec<u8> = vec![];
+    let mut bucket_edges: Vec<u8> = vec![255];
+
     let mut q: VecDeque<(u8, usize, usize)> = VecDeque::new();
     q.push_back((hist.median(0, 255), 0, 255));
 
-    let mut count = 1;
-    while !q.is_empty() && count < bucket_count {
+    loop {
         let (bucket_edge, left, right) = q.pop_front().unwrap();
         bucket_edges.push(bucket_edge);
 
+        if bucket_edges.len() == bucket_count {
+            break;
+        }
+
         // left: left to edge
-        q.push_back(
-            (
-                hist.median(left, bucket_edge as usize),
-                left,
-                bucket_edge as usize,
-            ),
-        );
+        q.push_back((
+            hist.median(left, bucket_edge as usize),
+            left,
+            bucket_edge as usize,
+        ));
 
         // right: edge to right
-        q.push_back(
-            (
-                hist.median(bucket_edge as usize, right),
-                bucket_edge as usize,
-                right,
-            ),
-        );
-
-        count += 1;
+        q.push_back((
+            hist.median(bucket_edge as usize, right),
+            bucket_edge as usize,
+            right,
+        ));
     }
 
     bucket_edges.sort_unstable();
 
+    // debug stuff
     println!("{:?}", bucket_edges);
+
+    let mut last = 0;
+    for edge in bucket_edges {
+        let mut sum = 0;
+        for index in last..=edge {
+            sum += hist.0[index as usize];
+        }
+        println!("{sum}");
+        last = edge;
+    }
 }
