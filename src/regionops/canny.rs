106f664954a3e945c83bf91455149047e29edbd2
Jonathan Kron <58877865+JonathanKr@users.noreply.github.com>
2024-06-03 13:43:32 +0200

    aaaaaaaaaa we need new data type

--- src/regionops/canny.rs
+++ src/regionops/canny.rs
@@ -12,28 +12,45 @@ impl CannyExt for GrayImage {
 }
 
 fn canny_gauss_filter(base_image: &GrayImage) -> GrayImage {
+    let norm_fac = 1f32 / 159f32;
+    #[rustfmt::skip]
+    let kernel = [
+        [2, 4, 5, 4, 2].as_slice(), 
+        [4, 9, 12, 9, 4].as_slice(), 
+        [5, 12, 15, 12, 5].as_slice(), 
+        [4, 9, 12, 9, 4].as_slice(), 
+        [2, 4, 5, 4, 2].as_slice()
+    ];
+
+    convolve(base_image, &kernel[..], norm_fac)
+}
+
+/// returns (Gx, Gy)
+fn canny_sobel(smooth_image: &GrayImage) -> (GrayImage, GrayImage) {
+    let (width, height) = smooth_image.dimensions();
+
+    let mut gx = convolve(smooth_image, &[&[1, 0, -1], &[2, 0, -2], &[1, 0, -1]], 1.);
+    let mut gy = smooth_image.clone();
+
+    (gx, gy)
+}
+
+fn convolve(base_image: &GrayImage, kernel: &[&[i32]], norm_fac: f32) -> GrayImage {
     let mut new_image = base_image.clone();
 
     let (width, height) = base_image.dimensions();
 
-    let norm_fac = 1f32 / 159f32;
-    #[rustfmt::skip]
-    let filter = [
-        [2, 4, 5, 4, 2], 
-        [4, 9, 12, 9, 4], 
-        [5, 12, 15, 12, 5], 
-        [4, 9, 12, 9, 4], 
-        [2, 4, 5, 4, 2]
-    ];
+    let half_filter_width_h = kernel[0].len() as i32 / 2;
+    let half_filter_width_v = kernel.len() as i32 / 2;
 
     new_image
         .par_enumerate_pixels_mut()
         .for_each(|(x, y, pixel)| unsafe {
             let mut sum: u32 = 0;
 
-            for dx in -2i32..=2 {
-                for dy in -2i32..=2 {
-                    sum += filter[(dy + 2) as usize][(dx + 2) as usize]
+            for dx in -half_filter_width_h..=half_filter_width_h {
+                for dy in -half_filter_width_v..=half_filter_width_v {
+                    sum += kernel[(dy + 2) as usize][(dx + 2) as usize]
                         * base_image.unsafe_get_pixel(
                             if dx.is_negative() {
                                 x.saturating_sub(dx.abs() as u32)
