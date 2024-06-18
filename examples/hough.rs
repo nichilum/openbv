165f1cc2c5683ee722e436a5018852a1e73fa8f0
Leo Kling <leo.kling.info@gmail.com>
2024-06-18 16:03:47 +0200

    b

--- examples/hough.rs
+++ examples/hough.rs
@@ -1,20 +1,23 @@
+use std::f32::consts::PI;
+
 use openbv::{
-    math::hough::{draw_lines, Line},
-    open_gray,
+    math::hough::{draw_lines, hough},
+    open_gray, regionops::canny::CannyExt,
 };
 
 fn main() {
-    let image = open_gray("./images/line.png").unwrap();
-    // let hough_lines = hough(&image, 1., PI / 180., 150);
-    // println!("{:?}", hough_lines);
+    let image = open_gray("./images/Set03_single.jpg").unwrap();
+    let canny = image.canny();
+    let hough_lines = hough(&canny, 1., PI / 180., 15);
+    println!("{:?}", hough_lines);
 
-    // let out_img = draw_lines(&image, &hough_lines);
-    let out_img = draw_lines(
-        &image,
-        &[Line {
-            r: 100.,
-            theta: 90f32.to_radians(),
-        }],
-    );
+    let out_img = draw_lines(&canny, &hough_lines);
+    // let out_img = draw_lines(
+    //     &image,
+    //     &[Line {
+    //         r: 100.,
+    //         theta: 90f32.to_radians(),
+    //     }],
+    // );
     out_img.save("export/line_hough.png").unwrap();
 }
--- src/math/hough.rs
+++ src/math/hough.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::PI;
-
 use image::{DynamicImage, GrayImage, RgbImage};
 
 use crate::binary_image::draw_line;
@@ -19,36 +17,48 @@ pub fn hough(image: &GrayImage, r_step: f32, theta_step: f32, threshold: u32) ->
     let max_r = (((width / 2).pow(2) + (height / 2).pow(2)) as f32).sqrt();
     let n_theta = (std::f32::consts::PI / theta_step).ceil() as u32;
     let n_r = (2. * max_r / r_step).ceil() as u32;
-    let mut acc = vec![0; n_r as usize * n_theta as usize];
+    let mut acc = vec![vec![0; n_theta as usize]; n_r as usize];
 
     image.enumerate_pixels().for_each(|(x, y, pixel)| {
         if pixel[0] != 255 {
             return;
         }
 
-        let (x, y) = (x as i32 - center.0 as i32, y as i32 - center.1 as i32);
+        //let (x, y) = (x as i32 - center.0 as i32, y as i32 - center.1 as i32);
         for i_theta in 0..n_theta {
             let theta = theta_step * i_theta as f32;
             let r = x as f32 * theta.cos() + y as f32 * theta.sin();
             let i_r = (n_r as f32 / 2. + (r / r_step).round()) as u32;
-            acc[i_r as usize * n_theta as usize + i_theta as usize] += 1;
+            acc[i_r as usize][i_theta as usize] += 1;
         }
     });
 
-    let line_params = acc
-        .iter()
-        .filter(|&&value| value >= threshold)
-        .enumerate()
-        .map(|(i, _)| {
-            let r = (i as u32 / n_theta) as f32 * r_step - max_r;
-            let theta = (i as u32 % n_theta) as f32 * theta_step;
+    // let line_params = acc
+    //     .iter()
+    //     .enumerate()
+    //     .filter(|(_, &value)| value >= threshold)
+    //     .map(|(i, _)| {
+    //         let r = (i as u32 / n_theta) as f32 * r_step - max_r;
+    //         let theta = (i as u32 % n_theta) as f32 * theta_step;
 
-            Line {
-                r: r.abs(),
-                theta: theta,
+    //         Line {
+    //             r,
+    //             theta,
+    //         }
+    //     })
+    //     .collect::<Vec<_>>();
+
+    let mut line_params = vec![];
+    for (r, row) in acc.iter().enumerate() {
+        for (theta, &value) in row.iter().enumerate() {
+            if value >= threshold {
+                let r = (r as u32 / n_theta) as f32 * r_step;
+                let theta = (theta as u32 % n_theta) as f32 * theta_step;
+
+                line_params.push(Line { r, theta });
             }
-        })
-        .collect::<Vec<_>>();
+        }
+    }
 
     line_params
 }
@@ -96,14 +106,24 @@ pub fn draw_lines(image: &GrayImage, lines: &[Line]) -> RgbImage {
             }
         }
 
-        let intersections = intersections
-            .iter()
+        // let intersections = intersections
+        //     .iter()
+        //     .fold(vec![], |mut acc, (x, y)| {
+        //         if !acc.contains(&(*x, *y)) {
+        //             acc.push((*x, *y));
+        //         }
+        //         acc
+        //     });
+        
+        let intersections = intersections.iter()
             .filter(|(x, y)| {
                 *x < image.width() as f32 && *x >= 0. && *y < image.height() as f32 && *y >= 0.
             })
             .collect::<Vec<&(f32, f32)>>();
 
-        assert!(intersections.len() == 2);
+        if intersections.len() < 2 {
+            continue;
+        }
 
         draw_line(
             &mut out_img,
@@ -113,12 +133,6 @@ pub fn draw_lines(image: &GrayImage, lines: &[Line]) -> RgbImage {
             intersections[1].1 as u32,
             image::Rgb([0, 255, 0]),
         );
-
-        let x1 = 0;
-        let y1 = (r / theta.cos()) as u32;
-        let x2 = image.width();
-        let y2 = ((r - x2 as f32 * theta.cos()) / theta.sin()) as u32;
-        draw_line(&mut out_img, x1, y1, x2, y2, image::Rgb([255, 0, 0]));
     }
 
     out_img
