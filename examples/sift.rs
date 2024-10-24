a49ffa0403e1a810b4f128cbad24d02704ba8634
JonathanKr <schule.jonathank@gmail.com>
2024-10-24 18:46:26 +0200

    uff

--- examples/sift.rs
+++ examples/sift.rs
@@ -1,14 +1,17 @@
 use opencv::{
-    core::{sort, KeyPoint, VecN, Vector},
-    features2d::{self, draw_keypoints, draw_matches, draw_matches_def, draw_matches_knn, BFMatcher},
+    core::{sort, DMatch, KeyPoint, VecN, Vector},
+    features2d::{
+        self, draw_keypoints, draw_matches, draw_matches_def, draw_matches_knn,
+        draw_matches_with_thickness_def, BFMatcher, FlannBasedMatcher,
+    },
     highgui, imgcodecs,
     prelude::{DescriptorMatcherTrait, DescriptorMatcherTraitConst, Feature2DTrait},
     Result,
 };
 
 fn main() -> anyhow::Result<()> {
-    let base_image = imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", 0)?;
-    let template = imgcodecs::imread("images/Vildkatten/VildkattenKarte01.png", 0)?;
+    let base_image = imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", 1)?;
+    let template = imgcodecs::imread("images/Vildkatten/VildkattenKarte01.png", 1)?;
 
     let mut sift = features2d::SIFT::create(0, 3, 0.04, 10., 1.6, false)?;
 
@@ -32,31 +35,42 @@ fn main() -> anyhow::Result<()> {
         false,
     )?;
 
-    // let flann_matcher = features2d::FlannBasedMatcher::new_def()?;
-    // let mut matches: Vector<Vector<opencv::core::DMatch>> = Vector::new();
-    // flann_matcher.knn_train_match(
-    //     &template_descriptors,
-    //     &base_descriptors,
-    //     &mut matches,
-    //     2,
-    //     &opencv::prelude::Mat::default(),
-    //     false
-    // )?;
-    // println!("Matches: {}", matches.len());
-
-    let mut out_image = imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", imgcodecs::IMREAD_COLOR)?;
-
-    let matcher = BFMatcher::new_def()?;
-
+    let matcher = FlannBasedMatcher::new_def()?;
     let mut matches = Vector::new();
-    matcher.train_match_def(&base_descriptors, &template_descriptors, &mut matches)?;
-    matches.as_mut_slice().sort_by(|a, b| a.distance.partial_cmp(&b.distance).unwrap());
+    let mut good_matches = Vector::<DMatch>::new();
 
-    let good_matches = matches.iter().take(10).collect::<Vec<_>>();
+    matcher.knn_train_match_def(&base_descriptors, &template_descriptors, &mut matches, 2)?;
+    for i in 0..matches.len() {
+        if matches.get(i).unwrap().get(0).unwrap().distance
+            < 0.75 * matches.get(i).unwrap().get(1).unwrap().distance
+        {
+            good_matches.push(matches.get(i).unwrap().get(0).unwrap());
+        }
+    }
 
+    // debug stuff
+    let mut out_image =
+        imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", imgcodecs::IMREAD_COLOR)?;
 
-
-    draw_matches_def(&base_image, &base_keypoints, &template, &template_keypoints, &good_matches.into(), &mut out_image)?;
+    // draw_keypoints(
+    //     &template,
+    //     &template_keypoints,
+    //     &mut out_image,
+    //     VecN::new(255., 0., 0., 0.),
+    //     features2d::DrawMatchesFlags::DRAW_RICH_KEYPOINTS,
+    // )?;
+    draw_matches(
+        &base_image,
+        &base_keypoints,
+        &template,
+        &template_keypoints,
+        &good_matches,
+        &mut out_image,
+        VecN::new(255., 255., 0., 255.),
+        VecN::new(255., 0., 0., 255.),
+        &Vector::new(),
+        features2d::DrawMatchesFlags::DRAW_RICH_KEYPOINTS,
+    )?;
     highgui::named_window("hello opencv!", 0)?;
     highgui::imshow("hello opencv!", &out_image)?;
     highgui::wait_key(0)?;
