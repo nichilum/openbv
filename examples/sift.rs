1d3c9079ee931f74f08da1dcd581687bebc90f68
JonathanKr <schule.jonathank@gmail.com>
2024-10-25 21:19:21 +0200

    homo stuff

--- examples/sift.rs
+++ examples/sift.rs
@@ -1,35 +1,35 @@
 use opencv::{
     calib3d,
-    core::{DMatch, KeyPoint, KeyPointTraitConst, MatTraitConst, Point2f, Vector},
-    features2d::{self, draw_matches_def, FlannBasedMatcher},
-    highgui, imgcodecs::{self, IMREAD_GRAYSCALE},
-    prelude::{DescriptorMatcherTraitConst, Feature2DTrait}, xfeatures2d::SURF,
+    core::{DMatch, KeyPoint, KeyPointTraitConst, MatTraitConst, Point2f, VecN, Vector},
+    features2d::{self, draw_keypoints, draw_matches_def, FlannBasedMatcher},
+    highgui,
+    imgcodecs::{self, IMREAD_GRAYSCALE},
+    prelude::{DescriptorMatcherTraitConst, Feature2DTrait},
 };
 
 fn main() -> anyhow::Result<()> {
-    let base_image = imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", IMREAD_GRAYSCALE)?;
-    let template = imgcodecs::imread("images/Vildkatten/VildkattenKarte02.png", IMREAD_GRAYSCALE)?;
+    let query_image =
+        imgcodecs::imread("images/Vildkatten/VildkattenKarte02.png", IMREAD_GRAYSCALE)?;
+    let train_image = imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", IMREAD_GRAYSCALE)?;
 
-    // let mut sift = features2d::SIFT::create(0, 3, 0.04, 10., 1.6, false)?;
-    let mut sift = SURF::create_def()?;
-
-    let mut base_keypoints = Vector::<KeyPoint>::new();
-    let mut base_descriptors = opencv::prelude::Mat::default();
+    let mut sift = features2d::SIFT::create(0, 3, 0.04, 10., 1.6, false)?;
+    let mut query_keypoints = Vector::<KeyPoint>::new();
+    let mut query_descriptors = opencv::prelude::Mat::default();
     sift.detect_and_compute(
-        &base_image,
+        &query_image,
         &opencv::prelude::Mat::default(),
-        &mut base_keypoints,
-        &mut base_descriptors,
+        &mut query_keypoints,
+        &mut query_descriptors,
         false,
     )?;
 
-    let mut template_keypoints = Vector::<KeyPoint>::new();
-    let mut template_descriptors = opencv::prelude::Mat::default();
+    let mut train_keypoints = Vector::<KeyPoint>::new();
+    let mut train_descriptors = opencv::prelude::Mat::default();
     sift.detect_and_compute(
-        &template,
+        &train_image,
         &opencv::prelude::Mat::default(),
-        &mut template_keypoints,
-        &mut template_descriptors,
+        &mut train_keypoints,
+        &mut train_descriptors,
         false,
     )?;
 
@@ -37,7 +37,7 @@ fn main() -> anyhow::Result<()> {
     let mut matches = Vector::new();
     let mut good_matches = Vector::<DMatch>::new();
 
-    matcher.knn_train_match_def(&base_descriptors, &template_descriptors, &mut matches, 2)?;
+    matcher.knn_train_match_def(&query_descriptors, &train_descriptors, &mut matches, 2)?;
     for i in 0..matches.len() {
         if matches.get(i).unwrap().get(0).unwrap().distance
             < 0.75 * matches.get(i).unwrap().get(1).unwrap().distance
@@ -46,33 +46,13 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    // debug stuff
-    let mut out_image =
-        imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", imgcodecs::IMREAD_COLOR)?;
-
-    // draw_keypoints(
-    //     &template,
-    //     &template_keypoints,
-    //     &mut out_image,
-    //     VecN::new(255., 0., 0., 0.),
-    //     features2d::DrawMatchesFlags::DRAW_RICH_KEYPOINTS,
-    // )?;
-    // draw_matches(
-    //     &good_matches,
-    //     &mut out_image,
-    //     VecN::new(255., 255., 0., 255.),
-    //     VecN::new(255., 0., 0., 255.),
-    //     &Vector::new(),
-    //     features2d::DrawMatchesFlags::DRAW_RICH_KEYPOINTS,
-    // )?;
-
     let src_points = good_matches
         .iter()
-        .map(|m| base_keypoints.get(m.query_idx as usize).unwrap().pt())
+        .map(|m| query_keypoints.get(m.query_idx as usize).unwrap().pt())
         .collect::<Vec<_>>();
     let dst_points = good_matches
         .iter()
-        .map(|m| template_keypoints.get(m.train_idx as usize).unwrap().pt())
+        .map(|m| train_keypoints.get(m.train_idx as usize).unwrap().pt())
         .collect::<Vec<_>>();
 
     let h = calib3d::find_homography(
@@ -80,27 +60,38 @@ fn main() -> anyhow::Result<()> {
         &Vector::<Point2f>::from(src_points),
         &mut opencv::prelude::Mat::default(),
         calib3d::RANSAC,
-        3.0,
+        5.0,
     )?;
+
+    println!("{:?}", h);
 
     let template_corners = Vector::<Point2f>::from(vec![
         Point2f::new(0.0, 0.0),
-        Point2f::new(template.cols() as f32, 0.0),
-        Point2f::new(template.cols() as f32, template.rows() as f32),
-        Point2f::new(0.0, template.rows() as f32),
+        Point2f::new(train_image.cols() as f32, 0.0),
+        Point2f::new(train_image.cols() as f32, train_image.rows() as f32),
+        Point2f::new(0.0, train_image.rows() as f32),
     ]);
     let mut template_corners_transformed = Vector::<Point2f>::new();
     opencv::core::perspective_transform(&template_corners, &mut template_corners_transformed, &h)?;
 
-    println!("{:?}", template_corners_transformed);
+    // println!("{:?}", template_corners_transformed);
 
-    draw_matches_def(
-        &base_image,
-        &base_keypoints,
-        &template,
-        &template_keypoints,
-        &good_matches.into(),
+    let mut out_image =
+        imgcodecs::imread("images/Vildkatten/Vildkatten.jpg", imgcodecs::IMREAD_COLOR)?;
+    // draw_matches_def(
+    //     &query_image,
+    //     &query_keypoints,
+    //     &train_image,
+    //     &train_keypoints,
+    //     &good_matches.into(),
+    //     &mut out_image,
+    // )?;
+    draw_keypoints(
+        &query_image,
+        &query_keypoints,
         &mut out_image,
+        VecN::new(0., 0., 255., 0.),
+        features2d::DrawMatchesFlags::DRAW_RICH_KEYPOINTS,
     )?;
     highgui::named_window("hello opencv!", 0)?;
     highgui::imshow("hello opencv!", &out_image)?;
